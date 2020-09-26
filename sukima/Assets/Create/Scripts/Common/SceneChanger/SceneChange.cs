920f611e74a05ba2fc612966badbb3ed86626abe
katsudon_660yen <ngnl.9u89@gmail.com>
2020-09-26 14:27:06 +0900

    ポストエフェクト追加

---
+++ sukima/Assets/Create/Scripts/Common/SceneChanger/SceneChange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public bool ChangeStart;
+    public string ToSceneName;//宛先のシーン名
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeStart = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChangeStart == true)
+        {
+            SceneManager.LoadScene(ToSceneName);
+        }
+    }
+}
+//シーンを変えたいときこれを書く
+//GameObject.Find("SceneChange").GetComponent<SceneChange>().ChangeStart = true;
