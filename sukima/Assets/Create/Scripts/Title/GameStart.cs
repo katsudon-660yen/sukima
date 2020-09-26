e7798b3c89ddc58796e88ae96172e0192603d06a
maki34 <makimaki.akiaki275@gmail.com>
2020-09-26 14:12:13 +0900

    タイトル完成

--- sukima/Assets/Create/Scripts/Title/GameStart.cs
+++ sukima/Assets/Create/Scripts/Title/GameStart.cs
@@ -4,9 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    public GameObject obj;
     public void OnClickStartButton()
     {
+       
+        Instantiate(obj, new Vector3(0.0f, 2.0f, 0.0f), Quaternion.identity);
         //ここに遷移させるシーン名
-        SceneManager.LoadScene("Result_Scene");
+        Invoke("Call", 0.5f);
+    }
+
+    public void Call()
+    {
+        SceneManager.LoadScene("Game_Scene");
     }
 }
