3910597584b7d32df970425d68114aecb568ded8
katsudon_660yen <ngnl.9u89@gmail.com>
2020-09-26 14:57:35 +0900

    ゲームシーン追加

---
+++ sukima/Assets/Create/Scripts/Game/TimeUI/TimeUI_Count.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeUI_Count : MonoBehaviour
+{
+    //カウントする時間
+    [SerializeField]
+    private float countTime = 60.0f;
+    
+    //Timeから最初に貰うtime
+    private float starttime = 0;
+
+    //計算用カウントの最大値
+    private float maxcount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = Time.time;
+        maxcount = countTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間の更新
+        {
+            float freamtime = (Time.time - starttime);
+            countTime = maxcount - freamtime;
+
+            //countTimeが0未満にならないように
+            if (countTime <= 0)
+            {
+                countTime = 0;
+            }
+        }
+    }
+
+    //時間を渡す
+    public float GetTime()
+    {
+        return countTime;
+    }
+
+    //カウンターを再設定
+    public void ResetTime(int settime)
+    {
+        starttime = Time.time;
+        maxcount = settime;
+    }
+}
