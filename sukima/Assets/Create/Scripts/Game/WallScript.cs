76204f368ccea32ff0e79a89e76ed4a620a36251
Yu24maki <43841336+Yu24maki@users.noreply.github.com>
2020-09-26 13:05:26 +0900

    壁を動かすスクリプトを作成

---
+++ sukima/Assets/Create/Scripts/Game/WallScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum MOVE_TYPE
+{
+    SIN,
+    SIN_MINUS
+};
+
+public class WallScript : MonoBehaviour
+{
+    [SerializeField]
+    float term = 0.0f;     // 周期
+
+    [SerializeField]
+    float amplitude = 0.0f;    // 振幅
+
+    float time;    // 経過時間
+
+    [SerializeField]
+    MOVE_TYPE moveType = MOVE_TYPE.SIN;
+
+    Vector3 basePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+
+        basePosition = gameObject.GetComponent<Transform>().position;
+        Debug.Log(basePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deltaPosition = new Vector3(0.0f,0.0f,0.0f);
+        switch(moveType)
+        {
+            case MOVE_TYPE.SIN:
+                deltaPosition = new Vector3(0.0f, amplitude * Mathf.Sin(2 * Mathf.PI * time / term), 0.0f);
+                break;
+            case MOVE_TYPE.SIN_MINUS:
+                deltaPosition = new Vector3(0.0f, -amplitude * Mathf.Sin(2 * Mathf.PI * time / term), 0.0f);
+                break;
+            default:
+                break;
+        }
+        gameObject.GetComponent<Transform>().position = basePosition + deltaPosition;
+
+        time += Time.deltaTime;
+
+    }
+}
---
+++ sukima/Assets/Create/Scripts/Game/WallScriptDirect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScriptDirect : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    float limitTop;
+
+    [SerializeField]
+    float limitBottom;
+
+    [SerializeField]
+    bool isUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = gameObject.GetComponent<Transform>().position;
+
+        if (isUp)
+        {
+            position.y += moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            position.y += -moveSpeed * Time.deltaTime;
+        }
+
+        gameObject.GetComponent<Transform>().position = position;
+
+        if (position.y < limitBottom) 
+        {
+            isUp = true;
+        }
+        else if(position.y > limitTop)
+        {
+            isUp = false;
+        }
+    }
+}
