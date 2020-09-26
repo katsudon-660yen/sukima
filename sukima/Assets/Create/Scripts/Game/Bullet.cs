1c311147461f4665d11d8fb21727e2c540aea672
Crocell94 <spartan202@gmail.com>
2020-09-26 13:13:04 +0900

    プレイヤー＆弾

---
+++ sukima/Assets/Create/Scripts/Game/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    //[SerializeField]
+    public float m_Drawtime = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Drawtime = 1.5f;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawRay(transform.position, hit.point, Color.green, 1.0f);
+
+            if (hit.collider.gameObject.GetComponent<BoxCollider2D>())
+            {
+
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        m_Drawtime -= Time.unscaledDeltaTime;
+
+        if (m_Drawtime <= 0)
+        {
+            Time.timeScale = 1;
+            Destroy(this.gameObject);
+        }
+    } 
+}
---
+++ sukima/Assets/Create/Scripts/Game/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Bullet m_Bullet;
+
+    [SerializeField, Space(20)]
+    private float m_FireRate = 0.5f;
+    private float m_FireCD;
+
+    void Start()
+    {
+        m_FireCD = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_FireCD > 0)
+        {
+            m_FireCD -= Time.deltaTime;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && m_FireCD == 0)
+        {
+            Bullet blt = Instantiate(m_Bullet);
+            blt.gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            m_FireCD = m_FireRate;
+            Time.timeScale = 0;
+        }
+    }
+}
